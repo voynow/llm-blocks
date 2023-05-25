453f79ae5a2b12eeb26b7b1d58c226d672416728
voynow <voynow99@gmail.com>
2023-05-25 17:35:44 -0400

    Eval insights via flatten_responses function

--- repo_chat/eval_utils.py
+++ repo_chat/eval_utils.py
@@ -1,15 +1,11 @@
 import concurrent.futures
-from repo_chat import templates
-from repo_chat.chat_utils import ChainManager, RetrievalChain
+import pandas as pd
+from repo_chat.chat_utils import  RetrievalChain
+from repo_chat import chain_manager
 
 class CriticChain:
     def __init__(self):
-        self.chain_manager = ChainManager()
-        self.chain = self.get_chain("CRITIC")
-
-    def get_chain(self, template_type):
-        """Get chain by passing the respective template type"""
-        return self.chain_manager.create_chain(**getattr(templates, template_type))
+        self.chain = chain_manager.get_chain("CRITIC")
 
     def score(self, query, response):
         """Score a given response to a query"""
@@ -22,15 +18,18 @@ class QueryEvaluator:
         self.get_vectorstore = get_vectorstore
         self.query = query
         self.runs_per_query = runs_per_query
-        self.chainlogs = []
 
     def score_response(self, chain, query):
         """Score a response using CriticChain"""
         critic = CriticChain()
         response = chain.chat(query)
         score = critic.score(query, response["text"])
-        response["score"] = score["text"]
-        return response
+        response['score'] = score['text']
+
+        return {
+            'chainlogs': chain.chainlogs,
+            'response': response
+        }
 
     def evaluate(self):
         """Evaluate RetrievalChain using CriticChain to score responses"""
@@ -40,7 +39,7 @@ class QueryEvaluator:
             chain = RetrievalChain(vectorstore)
             response = self.score_response(chain, self.query)
             responses.append(response)
-            self.chainlogs.append(chain.get_chatlog())
+
         return responses
 
 
@@ -70,9 +69,31 @@ class MultiQueryEvaluator:
                 responses[query] = response
         self.responses = responses
 
-    def get_scores(self):
-        """Return responses from all evaluators"""
-        scores = {}
-        for query, response in self.responses.items():
-            scores[query] = [resp["score"] for resp in response]
-        return scores
+
+    def flatten_responses(self):
+        """
+        Convert responses into two dataframes:
+        1. chainlogs_df: chainlogs for each query-response pair
+        2. response_df: data from each query-response pair
+        """
+        chainlogs_dfs = []
+        response_dfs = []
+
+        for key, data in self.responses.items():
+            for obj in data:
+
+                df_chainlogs = pd.json_normalize(obj['chainlogs'])
+                df_chainlogs['query'] = key
+                chainlogs_dfs.append(df_chainlogs)
+
+                df_response = pd.DataFrame([obj['response']])
+                df_response['similar_documents'] = df_response['similar_documents'].apply(lambda x: ','.join(x))
+                df_response['query'] = key
+                response_dfs.append(df_response)
+
+        chainlogs_df = pd.concat(chainlogs_dfs, ignore_index=True)
+        response_df = pd.concat(response_dfs, ignore_index=True)
+
+        return chainlogs_df, response_df
+
+
