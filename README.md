22cd3a958b0f91fbaad74a94f27079fbb6e41d97
voynow <voynow99@gmail.com>
2023-06-07 11:27:43 -0400

    Huge refactor for GenericChain - removing old code (temporarily)

--- README.md
+++ README.md
@@ -1,74 +1,67 @@
-# LLM Repo Assistant
+# LLM Blocks
 
-This is a repository for an AI-driven software engineering assistant that leverages the power of large language models (LLMs) in combination with document retrieval from GitHub repositories. The provided code manages chained AI models and provides a system for scoring answers to queries. The code is based on the "langchain" library and has additional utilities to create, manage, and evaluate AI chains in specific contexts.
+LLM Blocks is a Python module that helps users interact with Language Learning Model (LLM) chains. It provides a simple and flexible way to create and manage LLM chains, ensuring efficient interactions with models such as OpenAI's GPT-3.5 Turbo.
 
-## Requirements
+## Installation
+First, make sure to have Python installed. Then, to install the required dependencies for this module, run the following commands:
 
-To install the requirements for this project, please run
-
-```bash
+```
 pip install -r requirements.txt
 pip install -r requirements.dev.txt
 ```
 
-## Repository Overview
+## Configuration
+To run the LLM Blocks, you'll need an OpenAI API key. Store your API key in a .env file or export it as an environment variable:
 
-- `exclude.toml`: Exclude specific files or directories from the repository.
-- `requirements.dev.txt`: Development-related dependencies.
-- `requirements.txt`: Main dependencies for running the project.
-- `llm_blocks/chain_manager.py`: A module to manage creating and storing chains and OpenAI credentials.
-- `llm_blocks/chat_utils.py`: Utilities for chatting with LLM and document retrieval from a GitHub repo.
-- `llm_blocks/eval_utils.py`: Utilities to evaluate chains and queries and provide user scores.
-- `llm_blocks/templates.py`: Template definitions used in the provided chains.
+For a .env file:
+```
+OPENAI_API_KEY=your_openai_api_key
+```
 
-## Workflow
+For environment variables:
+```
+export OPENAI_API_KEY=your_openai_api_key
+```
 
-1. Initialize a custom chain:
+## Usage
+The `llm_blocks` folder contains the main ChatUtils class, which can be utilized to create and manage your LLM chains.
 
-    ```python
-    from llm_blocks.chain_manager import CustomChain
-    chain = CustomChain("UPGRADE_QUERY")
-    ```
+Here's a simple example of using the `GenericChain` class:
 
-2. Call the chain with input data:
+```python
+from llm_blocks.chat_utils import GenericChain
 
-    ```python
-    input_data = {
-        "query": "How do I install the dependencies?",
-        "repo": "user/repo"
-    }
-    chain_response = chain(input_data)
-    ```
+# Create a chain with a given template
+template = "The meaning of {word} is:"
+my_chain = GenericChain(template)
 
-3. Chat with an AI-driven software engineering assistant:
+# Call the chain with any input you desire
+response = my_chain("friendship")
+print(response)
+```
 
-    ```python
-    from llm_blocks.eval_utils import RetrievalChain
-    chat = RetrievalChain(vectorstore, repo)
-    answer = chat.chat("How do I install the dependencies?")
-    ```
+## Module Structure
+- `exclude.toml`: Configuration file to specify files or directories to exclude.
+- `requirements.dev.txt`: Development dependencies for this module.
+- `requirements.txt`: Main dependencies for this module.
+- `llm_blocks`
+  - `chat_utils.py`: Python file containing the definition of the `GenericChain` class and utility functions for working with LLM chains.
 
-4. Use raw chain for processing raw code input:
+## Logging Responses
+The `GenericChain` class keeps a log of all interactions with the given LLM. The log is stored as a list of dictionaries and can be accessed with `my_chain.logs`.
 
-    ```python
-    from llm_blocks.chat_utils import RawChain
-    raw_code = """
-    import pandas as pd
-    df = pd.read_csv('data.csv')
-    """
-    chat = RawChain(raw_code)
-    answer = chat.chat("What library is being imported?")
-    ```
+Example:
+```python
+for log in my_chain.logs:
+    print(f'Inputs: {log["inputs"]}')
+    print(f'Callback: {log["callback"]}')
+    print(f'Response: {log["response"]}')
+    print(f'Response Time: {log["response_time"]}')
+    print('---')
+```
 
-5. Evaluate chains:
+## Contributing
+Feel free to submit pull requests, report bugs, or suggest new features through the GitHub repository. We appreciate your contributions and feedback!
 
-    ```python
-    from llm_blocks.eval_utils import MultiQueryEvaluator
-    evaluator = MultiQueryEvaluator(get_vectorstore, queries, repo, runs_per_query=5)
-    evaluator.evaluate(max_workers=4)
-    response_df, chainlogs_df = evaluator.flatten_responses()
-    ```
-
-## Customization
-
-You can create new AI chains or modify the existing AI chains in the `llm_blocks/templates.py`. Define input variables and templates for the specific chain you want to create or modify.
+## License
+This project is licensed under the MIT License.
--- llm_blocks/chat_utils.py
+++ llm_blocks/chat_utils.py
@@ -1,129 +1,53 @@
-import json
+import dotenv
 from langchain.callbacks import get_openai_callback
-from llm_blocks import chain_manager
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import time
 
+dotenv.load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+if not OPENAI_API_KEY:
+    raise ValueError(
+        "OPENAI_API_KEY not found. Either create a .env file or set the environment variable."
+    )
 
-class RetrievalChain:
-    """
-    A class for chatting with a large language model using document retrieval from a GitHub repo.
-    It leverages the langchain library to facilitate the retrieval and chat operations.
-    """
 
-    def __init__(self, vectorstore, repo):
-        """
-        Initialize the class with the given vectorstore
-        """
-        self.vectorstore = vectorstore
-        self.repo = repo
-        self.chainlogs = []
-
-    def log_entry(self, chain_name, input_data, output_data, exec_time):
-        """Log the given method, input_data, output_data and execution time"""
-        self.chainlogs.append({
-            "chain_name": chain_name,
-            "input_data": json.dumps(input_data),
-            "output_data": output_data['text'],
-            "execution_time": exec_time
-        })
-
-    def get_chain_inputs(self, query, get_docs=True):
-        """
-        Retrieve similar documents from vectorstore based on the given query.
-        Prepare the chain inputs using these documents.
-        """
-        chain_inputs = {
-            "query": query,
-            "repo": self.repo,
-        }
-        if get_docs:
-            docs = self.vectorstore.similarity_search_with_score(query, k=5)
-            chain_inputs["similar_documents"] = [doc.page_content for doc, _ in docs]
-        return chain_inputs
-    
-    def call_chain(self, chain, input_data):
-        """Call the given chain and log the execution time"""
-        start_time = time.time()
-        output = chain(input_data)
-        exec_time = time.time() - start_time
-        self.log_entry(chain.name, input_data, output, exec_time)
-        return output
-
-    def process_query(self, query, context_validator, run_query, context_threshold=60):
-        """
-        Process the given query using the context_validator and run_query chains.
-        If sufficient context is present, return the result of run_query.
-        """
-        chain_inputs = self.get_chain_inputs(query)
-        val_resp = self.call_chain(context_validator, chain_inputs)
-        sufficient_context = int(val_resp["text"].lower())
-        if sufficient_context > context_threshold:
-            return self.call_chain(run_query, chain_inputs)
-        else:
-            return None
-
-    def iterate_through_queries(
-        self, upgrade_query, query, context_validator, run_query, context_threshold
+class GenericChain:
+    def __init__(
+        self, template: str, model_name: str = "gpt-3.5-turbo", temperature: float = 0.2
     ):
-        """
-        For each query retrieved from the upgrade_query response,
-        process the query and return the answer if found.
-        """
-        chain_inputs = self.get_chain_inputs(query, get_docs=False)
-        upgrade_query_resp = self.call_chain(upgrade_query, chain_inputs)
-        for q in upgrade_query_resp["text"].split("\n"):
-            refined_query = q.lstrip("0123456789. ")
-            answer = self.process_query(
-                refined_query, context_validator, run_query, context_threshold
-            )
-            if answer is not None:
-                return answer
-        return None
+        llm = ChatOpenAI(
+            openai_api_key=OPENAI_API_KEY,
+            model_name=model_name,
+            temperature=temperature,
+        )
+        prompt = PromptTemplate.from_template(template)
+        self.chain = LLMChain(llm=llm, prompt=prompt)
+        self.logs = []
 
-    def manage_workflow(self, query, context_validator, run_query):
-        """Manage workflow for upgrading the query and processing it"""
-        context_threshold = 60
-        upgrade_query = chain_manager.get_chain("UPGRADE_QUERY")
-
-        while True:
-            answer = self.iterate_through_queries(
-                upgrade_query, query, context_validator, run_query, context_threshold
-            )
-            if answer is not None or context_threshold <= 0:
-                break
-            context_threshold -= 10
-        return answer
-
-    def chat(self, query):
-        """
-        Begin chat with the QA chain
-        Initialize context_validator and run_query chains.
-        """
-        context_validator = chain_manager.get_chain("CONTEXT_VALIDATOR")
-        run_query = chain_manager.get_chain("RUN_QUERY_RAG")
-
-        with get_openai_callback() as cb:
-            answer = self.process_query(query, context_validator, run_query)
-            if answer is not None:
-                return answer
-            else:
-                return self.manage_workflow(query, context_validator, run_query)
-
-
-class RawChain:
-    """ Chain specific for raw code input """
-    def __init__(self, repo_data):
-        """ Initialize the basic chain object """
-        self.repo_data = repo_data
-
-    def chat(self, query):
-        """ Chat with the basic chain object """
-        raw_chain = chain_manager.get_chain("RAW_CODE")
-
-        with get_openai_callback() as cb:
-            chain_inputs = {
-                "query": query,
-                "repo_data": self.repo_data,
+    def __call__(self, *args, **kwargs):
+        inputs = {}
+        if args:
+            inputs = {
+                key: value
+                for key, value in zip(self.chain.prompt.input_variables, args)
             }
-            output = raw_chain(chain_inputs)
-            return output
+        if kwargs:
+            inputs.update(kwargs)
+
+        start_time = time.time()
+        with get_openai_callback() as cb:
+            response = self.chain(inputs)
+        response_time = time.time() - start_time
+
+        # Store logs in a list of dictionaries
+        log_entry = {
+            "inputs": inputs,
+            "callback": cb,
+            "response": response,
+            "response_time": response_time,
+        }
+        self.logs.append(log_entry)
+        return response
